4aea58a0dcb507f0b605fb1e32da0dffef232e71
samfinity <samsonpetros@outlook.com>
2025-10-08 17:24:57 +0200

    first commit client avec nom et prenom

---
+++ NintendoProject/src/main/java/nintendo/model/Client.java
@@ -0,0 +1,28 @@
+package nintendo.model;
+
+public class Client {
+	private String nom;
+	private String prenom;
+	public Client(String nom, String prenom) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+	}
+	public String getNom() {
+		return nom;
+	}
+	public void setNom(String nom) {
+		this.nom = nom;
+	}
+	public String getPrenom() {
+		return prenom;
+	}
+	public void setPrenom(String prenom) {
+		this.prenom = prenom;
+	}
+	@Override
+	public String toString() {
+		return "Client [nom=" + nom + ", prenom=" + prenom + "]";
+	}
+	
+}
