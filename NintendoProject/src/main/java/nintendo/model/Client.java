316d2ee857ef2cb1ab1311db2be1df611196febf
TReigner <thibaud.reigner@gmail.com>
2025-10-09 10:47:25 +0200

    Modifs de Client et ajouts de tests

--- NintendoProject/src/main/java/nintendo/model/Client.java
+++ NintendoProject/src/main/java/nintendo/model/Client.java
@@ -1,17 +1,28 @@
 package nintendo.model;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Client {
 	private String nom;
 	private String prenom;
-	private List<Jeu> listeAchats;
+	private List<Jeu> listeAchats = new ArrayList<>();
 	
 	public Client(String nom, String prenom) {
 		super();
 		this.nom = nom;
 		this.prenom = prenom;
 	}
+	
+	public Client(String nom, String prenom, List<Jeu> listeAchats) {
+		super();
+		this.nom = nom;
+		this.prenom = prenom;
+		this.listeAchats = listeAchats;
+	}
+
+
+
 	public String getNom() {
 		return nom;
 	}
@@ -24,9 +35,22 @@ public class Client {
 	public void setPrenom(String prenom) {
 		this.prenom = prenom;
 	}
+	public List<Jeu> getListeAchats() {
+		return listeAchats;
+	}
+	public void setListeAchats(List<Jeu> listeAchats) {
+		this.listeAchats = listeAchats;
+	}
+
+	
+	
 	@Override
 	public String toString() {
-		return "Client [nom=" + nom + ", prenom=" + prenom + "]";
+		return "Client [nom=" + nom + ", prenom=" + prenom + ", listeAchats=" + listeAchats + "]";
+	}
+
+	public void addAchat(Jeu jeu) {
+		this.listeAchats.add(jeu);
 	}
 	
 }
--- NintendoProject/src/test/java/nintendo/test/Test.java
+++ NintendoProject/src/test/java/nintendo/test/Test.java
@@ -1,29 +1,38 @@
 package nintendo.test;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import nintendo.model.Adresse;
 import nintendo.model.Boutique;
 import nintendo.model.Client;
-import nintendo.model.Console;
+import nintendo.model.Hybride;
 import nintendo.model.Jeu;
+import nintendo.model.Portable;
+import nintendo.model.Salon;
 
 public class Test {
 
 	public static void main(String[] args) {
 		
-		Console console = new Console("Switch");
+		Hybride hybride = new Hybride("Switch", 300.0, LocalDate.now());
+		Portable portable = new Portable("Steam Deck", 550.0, LocalDate.now());
+		Salon salon = new Salon("PS5", 450.0, LocalDate.now());
 		
-		Jeu jeu = new Jeu("Mario Kart", console);
+		Jeu jeu = new Jeu("Mario Kart", hybride);
 		
 		Client client = new Client("John", "Doe");
 		Client client2 = new Client("Jane", "Doe");
 		Adresse adresse = new Adresse(10, "Avenue des Lilas", "Paris");
 		
 		Boutique boutique = new Boutique("Alerte Hit", adresse);
-		List<Jeu> achatsClient = new ArrayList<>();
-        achatsClient.add(jeu);
+		
+		List<Jeu> achatsClients = new ArrayList<>();
+        
+		client.addAchat(jeu);
+		System.out.println(client.toString());
+		
 		System.out.println("Test réussi");
 		
 		//samson
